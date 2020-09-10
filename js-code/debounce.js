e712940d15cd0aebe1a0a6f64d79dddd3e5957f3
perfectFu <fuaoqi@outlook.com>
2020-09-10 18:27:54 +0800

    修改节流函数

--- js-code/debounce.js
+++ js-code/debounce.js
@@ -1,14 +1,23 @@
 // 函数防抖
 
-function debounce(fn, wait) {
-  let timer
+/**
+ * 
+ * @param {*} fn 需要执行的函数
+ * @param {*} wait 防抖的间隔时间
+ * @param {*} immediate 第一次是否立即执行
+ */
+function debounce(fn, wait, immediate = false) {
+  let timer = null
   return (...args) => {
+    let now = immediate && !timer
     if(timer) {
       clearTimeout(timer)
     }
     timer = setTimeout(() => {
       fn.apply(null, args)
     }, wait)
+
+    now && fn.apply(null, args)
   }
 }
 
@@ -19,12 +28,13 @@ function debounce(fn, wait) {
  * @param {*} fn 
  * @param {*} wait 
  */
-function throttle(fn, wait) {
+function throttle(fn, wait, immediate = false) {
   let timer
   return function(...args) {
     if(!timer) {
       timer = setTimeout(() => {
         console.log('函数执行了');
+        fn.apply(null, args)
         clearTimeout(timer)
         timer = null
       }, wait)
@@ -34,13 +44,13 @@ function throttle(fn, wait) {
 
 
 // 测试代码
-const fn = throttle(() => {
+const fn = debounce(() => {
   console.log('函数执行了');
-}, 1000)
+}, 1000, true)
 
 let count = 0
 let timer = setInterval(() => {
-  if(count === 50) {
+  if(count === 5) {
     clearInterval(timer)
   }
   console.log('-------每500ms触发一次fn---------')
